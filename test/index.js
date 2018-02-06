79b9f2402c4a68f113adb6b97ae6cdfde1feb887
Lev Himmelfarb <lev@boylesoftware.com>
2018-02-06 16:40:36 -0500

    Implemented diffFrom functionality.

--- test/index.js
+++ test/index.js
@@ -19,11 +19,11 @@ const TEST_LIB = {
 					valueType: 'string'
 				},
 				'optionalSimpleProp': {
-					valueType: 'number',
+					valueType: 'string',
 					optional: true
 				},
 				'simpleArrayProp': {
-					valueType: 'number[]'
+					valueType: 'string[]'
 				},
 				'simpleMapProp': {
 					valueType: 'string{}'
@@ -68,10 +68,10 @@ describe('x2node-patches Module', function() {
 
 	describe('build()', function() {
 
-		it('build valid patch', function() {
+		it('builds valid patch', function() {
 			expect(patches.build(recordTypes, 'Record1', [
-				{ op: 'add', path: '/simpleArrayProp', value: [ 7, 10 ] },
-				{ op: 'replace', path: '/simpleArrayProp/0', value: 5 },
+				{ op: 'add', path: '/simpleArrayProp', value: [ 'A', 'B' ] },
+				{ op: 'replace', path: '/simpleArrayProp/0', value: 'C' },
 				{ op: 'remove', path: '/simpleArrayProp/0' },
 				{ op: 'move', path: '/simpleArrayProp/-', from: '/optionalSimpleProp' },
 				{ op: 'copy', path: '/simpleMapProp/key1', from: '/simpleProp' },
@@ -79,4 +79,88 @@ describe('x2node-patches Module', function() {
 			])).to.be.ok;
 		});
 	});
+
+	describe('fromDiff()', function() {
+
+		const rec = {
+			id: 1,
+			simpleProp: 'Mamber',
+			simpleArrayProp: [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ],
+			nestedObjProp: {
+				prop1: 'Zumber'
+			},
+			nestedObjArrayProp: [
+				{ id: 1, prop1: 'A' },
+				{ id: 2, prop1: 'B' },
+				{ id: 3, prop1: 'C' },
+				{ id: 4, prop1: 'D' },
+				{ id: 5, prop1: 'E' }
+			]
+		};
+
+		it('patches simple values array prop via diff', function() {
+			[
+				[ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ],
+				[ '0', '1' ],
+				[ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ],
+				[ 'A', 'B', 'C', '0', '1', 'D', 'E', 'F', 'G' ],
+				[ '0', '1', 'A', 'B', 'C', '2', '3', 'D', 'E', 'F', 'G', '4', '5' ],
+				[ 'A', 'B', 'E', 'F', 'G' ],
+				[ 'B', 'E', 'F' ],
+				[ 'A', 'D', 'E', 'G' ],
+				[ 'A', 'B', '0', '1', '2', '3', 'E', 'F', 'G' ],
+				[ 'A', 'B', '0', 'E', 'F', 'G' ],
+				[ 'A', 'B', '0', 'D', '1', 'F', '2' ]
+			].forEach(a => {
+				const recNew = deepCopy(rec);
+				recNew.simpleArrayProp = a;
+				const patchSpec = patches.fromDiff(
+					recordTypes, 'Record1', rec, recNew);
+				const patch = patches.build(recordTypes, 'Record1', patchSpec);
+				const recPatched = deepCopy(rec);
+				patch.apply(recPatched);
+				expect(recPatched).to.deep.equal(recNew);
+			});
+		});
+
+		it('patches nested objects array prop via diff', function() {
+			[
+				[
+					{ id: 1, prop1: 'A' },
+					{ id: 0, prop1: '0' },
+					{ id: 0, prop1: '1' },
+					{ id: 4, prop1: 'D' },
+					{ id: 5, prop1: 'E' },
+					{ id: 0, prop1: '2' }
+				]
+			].forEach(a => {
+				const recNew = deepCopy(rec);
+				recNew.nestedObjArrayProp = a;
+				const patchSpec = patches.fromDiff(
+					recordTypes, 'Record1', rec, recNew);
+				const patch = patches.build(recordTypes, 'Record1', patchSpec);
+				const recPatched = deepCopy(rec);
+				patch.apply(recPatched);
+				expect(recPatched).to.deep.equal(recNew);
+			});
+		});
+	});
 });
+
+function deepCopy(obj) {
+
+	let res;
+	if (Array.isArray(obj)) {
+		res = [];
+		for (let v of obj)
+			res.push(deepCopy(v));
+	} else if (((typeof obj) === 'object') && (obj !== null)) {
+		res = {};
+		for (let k of Object.keys(obj))
+			res[k] = deepCopy(obj[k]);
+	} else {
+		res = obj;
+	}
+
+	return res;
+}
